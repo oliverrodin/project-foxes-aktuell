9ece6c0bed4b4edd09deaa0019f645bd7a20f2fa
oliverrodin <79797556+oliverrodin@users.noreply.github.com>
2022-03-14 12:09:29 +0100

    design 1.1

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,37 +1,39 @@
-
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import './Report.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Report.css";
 
 function Home() {
-    const [reports, setReports] = useState([]);
+  const [reports, setReports] = useState([]);
 
-    useEffect( () => {
-      axios.post("http://localhost:3001/getdatabase/report").then((response) => {
-        setReports(response.data)
-      }) 
-    }, [])
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/getdatabase/report")
+      .then((response) => {
+        setReports(response.data);
+      });
+  }, []);
 
-    
-
-  
   return (
-    <div className='home-container'>
-      {console.log(reports)}
-      {
-        reports.map((value) => {
-          return(
+    <div className="home-section ">
+      <div className="header">
+        <h1 className="header-h1">Min Sida</h1>
+      </div>
+      <div className="left-container"></div>
+      <div className='report-container'>
+        {console.log(reports)}
+        {reports.map((value) => {
+          return (
             <div className='report-card'>
-              <ul key={value.id}>     
-                <li>Date: {value.date}</li>          
-                <li>Project: {value.projectName}</li>                
-              </ul>              
+              <ul key={value.id}>
+                <li>Date: {value.date}</li>
+                <li>Project: {value.projectName}</li>
+              </ul>
             </div>
-          )
-        })
-      }   
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
