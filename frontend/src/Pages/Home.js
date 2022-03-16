1464441ad928b9bc6e758a41a84b674af4685afd
rasmusekbom <rasmusekbom97@gmail.com>
2022-03-16 10:13:36 +0100

    Popup-modal till tidrapport

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -5,6 +5,12 @@ import "./Report.css";
 function Home() {
   const [reports, setReports] = useState([]);
   const [hej, setHej] = useState([]);
+  const [popupcontent, setpopupcontent] = useState([]);
+  const [popuptoggle, setpopuptoggle] = useState(false);
+  const changeContent=(value)=>{
+    setpopupcontent([value]);
+    setpopuptoggle(!popuptoggle);
+  };
 
   axios.post("http://localhost:3001/getdatabase/sendid")
          .then((res) => {
@@ -33,7 +39,7 @@ function Home() {
       <div className='report-container'>
         {reports.map((value) => {
           return (
-            <div className='report-card'>
+            <div className='report-card' onClick={()=>changeContent(value)}>
               <ul key={value.id}>
                 <li>Date: {value.date}</li>
                 <li>Project: {value.projectName}</li>
@@ -42,7 +48,28 @@ function Home() {
           );
         })}
       </div>
-      
+
+      {popuptoggle && 
+      <div className="pop-up-container" onClick={changeContent}>
+        <div className="pop-up-body" onClick={(e)=>e.stopPropagation()}>
+            <div className="pop-up-header">
+            <button onClick={changeContent}> X </button>
+          </div>
+          <div className="pop-up-content">
+            {popupcontent.map((pop)=>{
+              return (
+                <div className="pop-up-card">
+                  <p>Project: {pop.projectName}</p>
+                  <p>Date: {pop.date}</p>
+                  <p>Hours: {pop.hours}</p>
+                  
+                  <p>Note: {pop.note}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>}
     </div>
   );
 }
