86e47e2a71faea3eaeaa83a81990275f0d1876c5
rasmusekbom <rasmusekbom97@gmail.com>
2022-03-23 20:09:30 +0100

    create-report modalpopup 75% fungerande

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import axios from "axios";
 import "./Report.css";
 import { LoginContext } from '../Context/LoginContext'
 import "./Modal.css"; 
-
+import CustomSelect from "../Components/CustomSelect";
+import CustomTextInput from "../Components/CustomTextInput"
 import Navbar from './Navbar'
+import { Formik, Form, Field, ErrorMessage, useField } from "formik";
+import * as Yup from 'yup'
 
 function Home() {
   const [reports, setReports] = useState([]);
@@ -13,11 +16,17 @@ function Home() {
   const [hej, setHej] = useState([]);
   const [popupcontent, setpopupcontent] = useState([]);
   const [popuptoggle, setpopuptoggle] = useState(false);
+  const [popuptoggle2, setpopuptoggle2] = useState(false);
   const { name } = useContext(LoginContext);
   const changeContent=(value)=>{
     setpopupcontent([value]);
     setpopuptoggle(!popuptoggle);
   };
+  let navigate = useNavigate();
+
+  const changeContent2=()=>{
+    setpopuptoggle2(!popuptoggle2)
+  }
 
 
   
@@ -81,17 +90,12 @@ function Home() {
         
         <div className='report-container'>
 
-          <h2 className="report-header">Mina tidsrapporter <Link to="/createreport" className="link">
-              <i class="fa-solid fa-plus" ></i>
-            </Link>
+          <h2 className="report-header" >Mina tidsrapporter <i class="fa-solid fa-plus" onClick={()=>changeContent2(popuptoggle2)}></i>
           </h2>
           <table className="table-container">
               <thead className="table-head-home">
               <th>Datum</th>
                 <th>Project</th>
-                
-                
-                
               </thead>
               <tbody>
                 {
@@ -108,9 +112,6 @@ function Home() {
                 }
               </tbody>
             </table>
-        
-          
-        
         </div>
         
             {/* Pop-Up-Modalen för varje tidrapport */}
@@ -143,6 +144,82 @@ function Home() {
           </div>
         </div>}
       </div>
+              {/* Slut på Pop-Up-Modalen */}
+
+              {/* Pop-Up-Modalen för att skapa ny tidsrapport */}
+        {popuptoggle2 &&
+        <div className="pop-up-container" onClick={changeContent2}>
+          <div className="pop-up-body" onClick={(e)=>e.stopPropagation()}>
+              <div className="pop-up-header">
+              <button className="modalbtn" onClick={changeContent2}> X </button>
+            </div>
+            <div className="pop-up-content">
+            <Formik
+          initialValues={{
+            
+            project: "",
+            date: "",
+            hours: "",
+            note:"",
+            comment:""
+          }}
+          validationSchema={
+            Yup.object().shape({
+            project: Yup.string().required(),
+            date: Yup.date().required(),
+            hours:Yup.number().required(),
+            note: Yup.string().required(),
+            comment: Yup.string().required()
+          })}
+          onSubmit={(values, {setSubmitting, resetForm}) => {
+            axios
+              .post('http://localhost:3001/createtimereports', 
+                values,
+                {
+                  headers: {
+                    accessToken: sessionStorage.getItem("accessToken")
+                  }
+                }
+              )
+              
+              setTimeout(() => {
+                navigate("/home")
+                resetForm();
+                setSubmitting(false)
+              }, 2000)
+
+          }}
+          >
+          {props => (
+          <Form className="container">
+            <h1>Skapa ny tidsrapport</h1>
+            <CustomSelect className="timeReportInput" label="Projekt" name="project">
+              <option value="">Välj projekt</option>  
+              {
+                project.map((pages) => {
+                  return (
+                    <>
+                      <option value={pages.id}>{pages.name}</option> 
+                    </>
+                  )
+                  
+                })
+              }
+            </CustomSelect>   
+            <CustomTextInput className="timeReportInput" label="Datum" name="date" type="date"/>
+            <CustomTextInput className="timeReportInput" label="Timmar" name="hours" type="number" placeholder='ex. 10...'/>
+            <CustomTextInput className="timeReportInput" label="Anteckning" name="note" type="text" placeholder='ex. möte...'/>
+            <CustomTextInput className="timeReportInput" label="Kommentar" name="comment" type="text" placeholder='ex. löste denna bugg..'/>
+            
+            
+
+            <button type='submit' className="btnPos">{props.isSubmitting ? 'laddar...' : 'Skapa Tidsrapport'}</button>
+          </Form>
+          )}
+        </Formik>
+            </div>
+          </div>
+        </div>}
               {/* Slut på Pop-Up-Modalen */}
     
     </>
